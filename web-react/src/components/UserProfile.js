a9a1ccbbbe8f6f506d89f366ec7f6e26d003528c
ppecheux <pierrelouis.pecheux@gmail.com>
2020-09-15 15:08:37 +0200

    removed package-lock

--- web-react/src/components/UserProfile.js
+++ web-react/src/components/UserProfile.js
@@ -1,82 +1,76 @@
 import React, { useContext } from 'react'
-import { Link, useHistory } from 'react-router-dom';
-import {
-  Avatar,
-  Typography,
-  IconButton,
-  Grid
-} from '@material-ui/core'
+import { Link, useHistory } from 'react-router-dom'
+import { Avatar, Typography, IconButton, Grid } from '@material-ui/core'
 import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag';
-import EditIcon from '@material-ui/icons/Edit';
-import Skeleton from '@material-ui/lab/Skeleton';
+import gql from 'graphql-tag'
+import EditIcon from '@material-ui/icons/Edit'
+import Skeleton from '@material-ui/lab/Skeleton'
 import { UserContext } from '../UserContext'
 
-export default function UserProfile({ match: { params: { id } } }) {
+export default function UserProfile({
+  match: {
+    params: { id },
+  },
+}) {
   const { user: currentUser } = useContext(UserContext)
   let history = useHistory()
-  const { loading, error, data } = useQuery(gql`
-    query UserQuery($id: ID!) {
-      User(id: $id, first:1) {
-        id,
-        profilePicture,
-        name,
-        biography
+  const { loading, error, data } = useQuery(
+    gql`
+      query UserQuery($id: ID!) {
+        User(id: $id, first: 1) {
+          id
+          profilePicture
+          name
+          biography
+        }
       }
+    `,
+    {
+      variables: { id },
     }
-    `, {
-    variables: { id }
-  })
+  )
   const avatarStyle = { height: null }
-  let user;
+  let user
   if (loading || error || !data.User || !data.User.length) {
     if (error) {
       console.log(error)
-      return (null)
+      return null
     } else if (loading) {
       user = { loading: true }
     } else {
-      console.log("user has not been found in db")
-      history.push("/profile/" + currentUser.id)
+      console.log('user has not been found in db')
+      history.push('/profile/' + currentUser.id)
     }
   } else {
     user = data.User[0]
   }
 
   if (!user) {
-    history.push("/error/404 User not found");
+    history.push('/error/404 User not found')
     return null
   }
   return (
     <div>
-      <Grid container spacing={3}>
+      <Grid container justify="space-around">
         <Grid item xs>
-          {
-            user.loading
-              ? <Skeleton variant="circle"><Avatar /></Skeleton>
-              : <Avatar alt={user.name.toUpperCase()} src={user.profilePicture || "useAlt"} style={avatarStyle} />
-
-          }
+          {user.loading ? (
+            <Skeleton variant="circle">
+              <Avatar />
+            </Skeleton>
+          ) : (
+            <Avatar
+              alt={user.name.toUpperCase()}
+              src={user.profilePicture || 'useAlt'}
+              style={avatarStyle}
+            />
+          )}
         </Grid>
         <Grid item xs>
-          <Typography
-            variant="h4"
-            color="inherit"
-            noWrap
-          >
-            {
-              user.name
-                ? user.name
-                : user.loading
-                  ? <Skeleton />
-                  : "new rider"
-
-            }
+          <Typography variant="h4" color="inherit" noWrap>
+            {user.name ? user.name : user.loading ? <Skeleton /> : 'new rider'}
           </Typography>
         </Grid>
-        {
-          (currentUser.id === id)
-          &&
+        {currentUser.id === id && (
           <Grid item xs>
             <Link to="/editProfile">
               <IconButton
@@ -89,22 +83,17 @@ export default function UserProfile({ match: { params: { id } } }) {
               </IconButton>
             </Link>
           </Grid>
-        }
+        )}
       </Grid>
-      <Typography
-        color="inherit"
-        noWrap
-      >
-        {
+      <Typography color="inherit" noWrap>
+        {user.biography ? (
           user.biography
-            ? user.biography
-            : user.loading
-              ? <Skeleton />
-              : "Happy to join the askate community 😊"
-        }
+        ) : user.loading ? (
+          <Skeleton />
+        ) : (
+          'Happy to join the askate community 😊'
+        )}
       </Typography>
     </div>
   )
-
 }
-
